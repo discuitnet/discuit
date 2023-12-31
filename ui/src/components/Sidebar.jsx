39d32b95d06f60edd7c7e4219cb3b70fc8ec234a
Previnder <git@previnder.com>
2023-12-31 17:14:49 +0530

    UI: Sidebar communities list show more button

--- ui/src/components/Sidebar.jsx
+++ ui/src/components/Sidebar.jsx
@@ -41,6 +41,55 @@ const Sidebar = ({ isMobile = false }) => {
     return to;
   };
 
+  const [expanded, SetExpanded] = useState(false); // communities list
+  const renderCommunitiesList = () => {
+    const renderInitially = 10,
+      length = communities ? communities.length : 0,
+      lengthLeft = length - renderInitially;
+    return (
+      <>
+        {communities
+          .filter((_, i) => {
+            if (expanded) {
+              return true;
+            }
+            return i < renderInitially;
+          })
+          .map((c) => (
+            <Link
+              to={`/${c.name}`}
+              key={c.id}
+              className="sidebar-item with-image"
+              onClick={handleClose}
+            >
+              <CommunityProPic name={c.name} proPic={c.proPic} className="is-image is-no-hover" />
+              <span>{c.name}</span>
+            </Link>
+          ))}
+        {length > 10 && (
+          <div className="sidebar-item with-image" onClick={() => SetExpanded((prev) => !prev)}>
+            <svg
+              style={{
+                transform: expanded ? 'rotate(180deg)' : 'none',
+              }}
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.9995 16.8006C11.2995 16.8006 10.5995 16.5306 10.0695 16.0006L3.54953 9.48062C3.25953 9.19062 3.25953 8.71062 3.54953 8.42063C3.83953 8.13063 4.31953 8.13063 4.60953 8.42063L11.1295 14.9406C11.6095 15.4206 12.3895 15.4206 12.8695 14.9406L19.3895 8.42063C19.6795 8.13063 20.1595 8.13063 20.4495 8.42063C20.7395 8.71062 20.7395 9.19062 20.4495 9.48062L13.9295 16.0006C13.3995 16.5306 12.6995 16.8006 11.9995 16.8006Z"
+                fill="currentColor"
+              />
+            </svg>
+            <span>{expanded ? 'Show less' : `Show ${lengthLeft} more`}</span>
+          </div>
+        )}
+      </>
+    );
+  };
+
   return (
     <aside
       className={
@@ -216,17 +265,7 @@ const Sidebar = ({ isMobile = false }) => {
           </Link>
           */}
           <div className="sidebar-topic">{loggedIn ? 'My communities' : 'Communities'}</div>
-          {communities.map((c) => (
-            <Link
-              to={`/${c.name}`}
-              key={c.id}
-              className="sidebar-item with-image"
-              onClick={handleClose}
-            >
-              <CommunityProPic name={c.name} proPic={c.proPic} className="is-image is-no-hover" />
-              <span>{c.name}</span>
-            </Link>
-          ))}
+          {renderCommunitiesList()}
           {/*
           <div className="sidebar-topic">Social</div>
           <a
