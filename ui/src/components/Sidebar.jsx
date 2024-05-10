850beb6644d769db7ad245d9644eae5bedb385c8
Previnder <git@previnder.com>
2024-05-10 20:09:31 +0530

    UI: Sample sidebar list link

--- ui/src/components/Sidebar.jsx
+++ ui/src/components/Sidebar.jsx
@@ -280,6 +280,39 @@ const Sidebar = ({ isMobile = false }) => {
             My lists
           </Link>
           */}
+          <div className="sidebar-topic">My Lists</div>
+          <Link className="sidebar-item with-image" to="/about">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 5.25H14C13.59 5.25 13.25 4.91 13.25 4.5C13.25 4.09 13.59 3.75 14 3.75H21C21.41 3.75 21.75 4.09 21.75 4.5C21.75 4.91 21.41 5.25 21 5.25Z"
+                fill="currentColor"
+              />
+              <path
+                d="M21 10.25H14C13.59 10.25 13.25 9.91 13.25 9.5C13.25 9.09 13.59 8.75 14 8.75H21C21.41 8.75 21.75 9.09 21.75 9.5C21.75 9.91 21.41 10.25 21 10.25Z"
+                fill="currentColor"
+              />
+              <path
+                d="M21 15.25H3C2.59 15.25 2.25 14.91 2.25 14.5C2.25 14.09 2.59 13.75 3 13.75H21C21.41 13.75 21.75 14.09 21.75 14.5C21.75 14.91 21.41 15.25 21 15.25Z"
+                fill="currentColor"
+              />
+              <path
+                d="M21 20.25H3C2.59 20.25 2.25 19.91 2.25 19.5C2.25 19.09 2.59 18.75 3 18.75H21C21.41 18.75 21.75 19.09 21.75 19.5C21.75 19.91 21.41 20.25 21 20.25Z"
+                fill="currentColor"
+              />
+              <path
+                d="M7.92 3.5H5.08C3.68 3.5 3 4.18 3 5.58V8.43C3 9.83 3.68 10.51 5.08 10.51H7.93C9.33 10.51 10.01 9.83 10.01 8.43V5.58C10 4.18 9.32 3.5 7.92 3.5Z"
+                fill="currentColor"
+              />
+            </svg>
+
+            <span>Favorites</span>
+          </Link>
           <div className="sidebar-topic">{loggedIn ? 'My communities' : 'Communities'}</div>
           {renderCommunitiesList()}
           {/*
