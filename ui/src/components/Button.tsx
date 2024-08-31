c22b5bba5036ab3b8ae9774f8a43915423b47064
Previnder <git@previnder.com>
2024-08-31 18:34:21 +0530

    UI: All SVGs to be in one place

--- ui/src/components/Button.tsx
+++ ui/src/components/Button.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import React, { useRef } from 'react';
+import { SVGClose } from '../SVGs';
 import Spinner from './Spinner';
 
 type ButtonVariant = 'normal' | 'text';
@@ -72,21 +73,7 @@ export const ButtonClose = ({
   const cls = 'button-icon' + (className ? ` ${className}` : '');
   return (
     <Button className={cls} style={{ padding: '9px', ...style }} {...props}>
-      <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512.001 512.001"
-        xmlSpace="preserve"
-      >
-        <path
-          d="M284.286,256.002L506.143,34.144c7.811-7.811,7.811-20.475,0-28.285c-7.811-7.81-20.475-7.811-28.285,0L256,227.717
-			L34.143,5.859c-7.811-7.811-20.475-7.811-28.285,0c-7.81,7.811-7.811,20.475,0,28.285l221.857,221.857L5.858,477.859
-			c-7.811,7.811-7.811,20.475,0,28.285c3.905,3.905,9.024,5.857,14.143,5.857c5.119,0,10.237-1.952,14.143-5.857L256,284.287
-			l221.857,221.857c3.905,3.905,9.024,5.857,14.143,5.857s10.237-1.952,14.143-5.857c7.811-7.811,7.811-20.475,0-28.285
-			L284.286,256.002z"
-          strokeWidth={2}
-        />
-      </svg>
+      <SVGClose />
     </Button>
   );
 };
