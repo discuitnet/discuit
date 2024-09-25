1f0b7589dcc0c8cbb3a140c2b0452919cc83b4f6
Previnder <git@previnder.com>
2024-09-25 10:31:53 +0530

    UI: Fix comment "SHOW MORE" showing up incorrectly

--- ui/src/components/ShowMoreBox.jsx
+++ ui/src/components/ShowMoreBox.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const ShowMoreBox = ({ maxHeight = '300px', children, showButton = false, ...props }) => {
   const [overflowing, setOverflowing] = useState(false);
