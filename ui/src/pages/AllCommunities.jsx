e320cd75ac7f2ba8daf829180b73da1f229a3294
Previnder <git@previnder.com>
2024-10-11 17:26:05 +0530

    UI: Use the Feed component in /communities page

--- ui/src/pages/AllCommunities.jsx
+++ ui/src/pages/AllCommunities.jsx
@@ -1,27 +1,22 @@
 import PropTypes from 'prop-types';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import React, { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { ButtonClose } from '../components/Button';
 import CommunityProPic from '../components/CommunityProPic';
+import Feed from '../components/Feed';
 import { FormField } from '../components/Form';
 import { InputWithCount, useInputMaxLength } from '../components/Input';
 import MarkdownBody from '../components/MarkdownBody';
 import MiniFooter from '../components/MiniFooter';
 import Modal from '../components/Modal';
-import PageLoading from '../components/PageLoading';
 import ShowMoreBox from '../components/ShowMoreBox';
 import Sidebar from '../components/Sidebar';
 import { communityNameMaxLength } from '../config';
 import { mfetch, mfetchjson } from '../helper';
 import { useInputUsername } from '../hooks';
-import {
-  allCommunitiesUpdated,
-  loginPromptToggled,
-  snackAlert,
-  snackAlertError,
-} from '../slices/mainSlice';
+import { FeedItem } from '../slices/feedsSlice';
+import { loginPromptToggled, snackAlert, snackAlertError } from '../slices/mainSlice';
 import LoginForm from '../views/LoginForm';
 import JoinButton from './Community/JoinButton';
 
@@ -37,33 +32,29 @@ const AllCommunities = () => {
   const user = useSelector((state) => state.main.user);
   const loggedIn = user !== null;
 
-  const dispatch = useDispatch();
+  // const { items: comms, loading } = useSelector((state) => {
+  //   const names = state.main.allCommunities.items;
+  //   const communities = state.communities.items;
+  //   const items = [];
+  //   names.forEach((name) => items.push(communities[name]));
+  //   return {
+  //     items: items || [],
+  //     loading: state.main.allCommunities.loading,
+  //   };
+  // });
 
-  const { items: comms, loading } = useSelector((state) => {
-    const names = state.main.allCommunities.items;
-    const communities = state.communities.items;
-    const items = [];
-    names.forEach((name) => items.push(communities[name]));
+  const fetchCommunities = async (next) => {
+    const res = await mfetchjson('/api/communities');
+    const items = res.map((community) => new FeedItem(community, 'community', community.id));
     return {
-      items: items || [],
-      loading: state.main.allCommunities.loading,
+      items: items,
+      next: null,
     };
-  });
+  };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await mfetchjson('/api/communities');
-        dispatch(allCommunitiesUpdated(res));
-      } catch (error) {
-        dispatch(snackAlertError(error));
-      }
-    })();
-  }, []);
-
-  if (loading) {
-    return <PageLoading />;
-  }
+  const handleRenderItem = (item, index) => {
+    return <ListItem community={item.item} />;
+  };
 
   return (
     <div className="page-content page-comms wrap page-grid">
@@ -76,11 +67,11 @@ const AllCommunities = () => {
           </RequestCommunityButton>
         </div>
         <div className="comms-list">
-          {comms.map((community) => (
-            <CommItem key={community.id} itemKey={community.id}>
-              <ListItem key={community.id} community={community} />
-            </CommItem>
-          ))}
+          <Feed
+            feedId="all-communities"
+            onFetch={fetchCommunities}
+            onRenderItem={handleRenderItem}
+          />
         </div>
       </main>
       <aside className="sidebar-right">
@@ -92,50 +83,6 @@ const AllCommunities = () => {
         <CommunityCreationCard />
         <MiniFooter />
       </aside>
-    </div>
-  );
-};
-
-const heights = {};
-
-const CommItem = ({ itemKey, children }) => {
-  const [ref, inView] = useInView({
-    // rootMargin: '200px 0px',
-    threshold: 0,
-    initialInView: itemKey < 10,
-  });
-
-  const [beenInView, setBeenInView] = useState(inView);
-  useEffect(() => {
-    if (inView) {
-      setBeenInView(true);
-    }
-  }, [inView]);
-
-  const [height, setHeight] = useState(heights[itemKey] ?? 0);
-  const innerRef = useCallback((node) => {
-    if (node !== null && inView) {
-      const child = node.firstChild;
-      if (child) {
-        const { height } = child.getBoundingClientRect();
-        setHeight(height);
-      }
-    }
-  });
-  useEffect(() => {
-    if (height > 0) {
-      heights[itemKey] = height;
-    }
-  }, [height]);
-
-  let h = height;
-  if (h < 100) h = 100;
-
-  return (
-    <div className="comm-item" ref={ref}>
-      <div ref={innerRef} style={{ height: `${h}px` }}>
-        {(inView || beenInView) && children}
-      </div>
     </div>
   );
 };
@@ -248,7 +195,7 @@ RequestCommunityButton.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-const ListItem = ({ community }) => {
+const ListItem = React.memo(function ListItem({ community }) {
   const to = `/${community.name}`;
 
   const history = useHistory();
@@ -295,4 +242,4 @@ const ListItem = ({ community }) => {
       </div>
     </div>
   );
-};
+});
