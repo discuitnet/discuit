03eaee4408920775727bd23d8d8a60b7d5903b7d
Previnder <git@previnder.com>
2025-02-20 14:15:43 +0530

    Redirect www. to non-www host

--- cli/serve/serve.go
+++ cli/serve/serve.go
@@ -121,12 +121,10 @@ func serve(ctx *cli.Context) error {
 		Addr: conf.Addr,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if hostname := conf.Hostname(); hostname != "" {
-				// Redirect all requests to any subdomains of conf.Addr to
-				// conf.Addr (no redirecting is done if the hostname provided in
-				// config is empty).
-				if _, subdomain := strings.CutSuffix(r.Host, "."+conf.Hostname()); subdomain {
+				// Redirect all www. requests to a non-www. host.
+				if withoutWWW, found := strings.CutPrefix(r.Host, "www."); found {
 					url := *r.URL
-					url.Host = hostname
+					url.Host = withoutWWW
 					if https {
 						url.Scheme = "https"
 					} else {
