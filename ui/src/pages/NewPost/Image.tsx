1d6cd196a652c7987ca90968ed4d6d996056dadf
Previnder <git@previnder.com>
2025-09-19 03:47:46 +0530

    UI: Modal for adding alt text

--- ui/src/pages/NewPost/Image.tsx
+++ ui/src/pages/NewPost/Image.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx';
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { ButtonClose } from '../../components/Button';
+import Button, { ButtonClose } from '../../components/Button';
 import Img from '../../components/Image';
+import Modal from '../../components/Modal';
 import Textarea from '../../components/Textarea';
-import { useWindowWidth } from '../../hooks';
+import { useIsMobile, useWindowWidth } from '../../hooks';
 import { Image as ServerImage } from '../../serverTypes';
 
 export interface ImageProps {
@@ -47,6 +48,21 @@ const Image = ({
     setAltText(image.altText || '');
   }, [image.altText]);
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleModalClose = () => setModalOpen(false);
+  const handleModalCancel = () => {
+    setAltText(image.altText || '');
+    handleModalClose();
+  };
+  const handleAltTextSave = () => {
+    if (altText !== image.altText) {
+      onAltTextSave(altText);
+    }
+    handleModalClose();
+  };
+
+  const isMobile = useIsMobile();
+
   return (
     <div className="page-new-image" ref={divref}>
       {!disabled && (
@@ -55,6 +71,11 @@ const Image = ({
           onClick={() => onClose()}
           style={{ padding: '6px' }}
         />
+      )}
+      {(!disabled || isEditMode) && (
+        <Button className="button-alt-text" onClick={() => setModalOpen(true)}>
+          Alt text
+        </Button>
       )}
       <div className="contain-image">
         <Img
@@ -67,22 +88,38 @@ const Image = ({
           }}
         />
       </div>
-      {/* alt text input */}
-      {(!disabled || isEditMode) && (
-        <Textarea
-          className={clsx('page-new-image-alt', missingAltText && 'is-error')}
-          placeholder={'Describe this image (alt text)…'}
-          value={altText}
-          onChange={(e) => setAltText(e.target.value)}
-          onBlur={() => {
-            if (altText !== image.altText) {
-              onAltTextSave(altText);
-            }
-          }}
-          maxLength={1024}
-          style={{ marginTop: 8, width: '100%', resize: 'vertical' }}
-        />
-      )}
+      <Modal open={modalOpen} onClose={handleModalClose} noOuterClickClose>
+        <div className="modal-card modal-add-alt-text">
+          <div className="modal-card-head">
+            <div className="modal-card-title">Add alt text</div>
+            <ButtonClose onClick={handleModalClose} />
+          </div>
+          <div className="modal-card-content">
+            <Img
+              src={image.url}
+              backgroundColor={image.averageColor}
+              alt={image.altText || 'Just uploaded'}
+              style={{
+                width: isMobile ? '100%' : imgWidth,
+                height: isMobile ? 'auto' : imgHeight,
+              }}
+            />
+            <Textarea
+              className={clsx('page-new-image-alt', missingAltText && 'is-error')}
+              placeholder={'Describe this image (alt text)…'}
+              value={altText}
+              onChange={(e) => setAltText(e.target.value)}
+              maxLength={1024}
+            />
+          </div>
+          <div className="modal-card-actions">
+            <button className="button-main" onClick={handleAltTextSave}>
+              Save
+            </button>
+            <button onClick={handleModalCancel}>Cancel</button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
