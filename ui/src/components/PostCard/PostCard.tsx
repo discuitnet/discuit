3d84361943e2e7025f51ec0f95f8686a34810683
Previnder <git@previnder.com>
2025-10-23 07:05:15 +0530

    UI: Admin dashboard sidebar icons

--- ui/src/components/PostCard/PostCard.tsx
+++ ui/src/components/PostCard/PostCard.tsx
@@ -5,7 +5,7 @@ import { mfetchjson, omitWWWFromHostname, stringCount } from '../../helper';
 import { useIsMobile } from '../../hooks';
 import { snackAlertError } from '../../slices/mainSlice';
 import { Post, postHidden } from '../../slices/postsSlice';
-import { SVGExternalLink } from '../../SVGs';
+import { SVGComment, SVGExternalLink } from '../../SVGs';
 import Button from '../Button';
 import Link from '../Link';
 import MarkdownBody from '../MarkdownBody';
@@ -241,26 +241,7 @@ const PostCard = ({
         <div className="post-card-bottom">
           <div className="left">
             <Link to={postURL} className="button button-text button-with-icon" target={target}>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 22.81C11.31 22.81 10.66 22.46 10.2 21.85L8.7 19.85C8.67 19.81 8.55 19.76 8.5 19.75H8C3.83 19.75 1.25 18.62 1.25 13V8C1.25 3.58 3.58 1.25 8 1.25H16C20.42 1.25 22.75 3.58 22.75 8V13C22.75 17.42 20.42 19.75 16 19.75H15.5C15.42 19.75 15.35 19.79 15.3 19.85L13.8 21.85C13.34 22.46 12.69 22.81 12 22.81ZM8 2.75C4.42 2.75 2.75 4.42 2.75 8V13C2.75 17.52 4.3 18.25 8 18.25H8.5C9.01 18.25 9.59 18.54 9.9 18.95L11.4 20.95C11.75 21.41 12.25 21.41 12.6 20.95L14.1 18.95C14.43 18.51 14.95 18.25 15.5 18.25H16C19.58 18.25 21.25 16.58 21.25 13V8C21.25 4.42 19.58 2.75 16 2.75H8Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M17 8.75H7C6.59 8.75 6.25 8.41 6.25 8C6.25 7.59 6.59 7.25 7 7.25H17C17.41 7.25 17.75 7.59 17.75 8C17.75 8.41 17.41 8.75 17 8.75Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M13 13.75H7C6.59 13.75 6.25 13.41 6.25 13C6.25 12.59 6.59 12.25 7 12.25H13C13.41 12.25 13.75 12.59 13.75 13C13.75 13.41 13.41 13.75 13 13.75Z"
-                  fill="currentColor"
-                />
-              </svg>
+              <SVGComment variant="outline" />
               <span>{stringCount(post.noComments, false, 'comment')}</span>
               {post.newComments != 0 && (
                 <span className="post-new-comment-label">({post.newComments} new)</span>
--- ui/src/pages/AdminDashboard/Sidebar.tsx
+++ ui/src/pages/AdminDashboard/Sidebar.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import { MouseEvent } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
+import { SVGComment, SVGCommunities, SVGSettings } from '../../SVGs';
 
 function Sidebar({
   className,
@@ -31,26 +32,90 @@ function Sidebar({
     <aside className={clsx('sidebar sidebar-left is-custom-scrollbar is-v2', className)}>
       <div className="sidebar-content">
         <div className="sidebar-list" onClick={handleListClick}>
-          <Link className="sidebar-item" to={url}>
-            Settings
+          <Link className="sidebar-item with-image" to={url}>
+            <SVGSettings />
+            <span>Settings</span>
           </Link>
-          <Link className="sidebar-item" to={dashboardLink('users')}>
+          <Link className="sidebar-item with-image" to={dashboardLink('users')}>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z"
+                fill="currentColor"
+              />
+              <path
+                d="M12.0002 14.5C6.99016 14.5 2.91016 17.86 2.91016 22C2.91016 22.28 3.13016 22.5 3.41016 22.5H20.5902C20.8702 22.5 21.0902 22.28 21.0902 22C21.0902 17.86 17.0102 14.5 12.0002 14.5Z"
+                fill="currentColor"
+              />
+            </svg>
             Users
           </Link>
-          <Link className="sidebar-item" to={dashboardLink('comments')}>
-            Comments
+          <Link className="sidebar-item with-image" to={dashboardLink('comments')}>
+            <SVGComment />
+            <span>Comments</span>
           </Link>
-          <Link className="sidebar-item" to={dashboardLink('communities')}>
-            Communities
+          <Link className="sidebar-item with-image" to={dashboardLink('communities')}>
+            <SVGCommunities />
+            <span>Communities</span>
           </Link>
-          <Link className="sidebar-item" to={dashboardLink('ipblocks')}>
-            IP blocks
+          <Link className="sidebar-item with-image" to={dashboardLink('ipblocks')}>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21.12 6.98L17.02 2.88C16.54 2.4 15.58 2 14.9 2H9.1C8.42 2 7.46 2.4 6.98 2.88L2.88 6.98C2.4 7.46 2 8.42 2 9.1V14.9C2 15.58 2.4 16.54 2.88 17.02L6.98 21.12C7.46 21.6 8.42 22 9.1 22H14.9C15.58 22 16.54 21.6 17.02 21.12L21.12 17.02C21.6 16.54 22 15.58 22 14.9V9.1C22 8.42 21.6 7.46 21.12 6.98ZM16.03 14.97C16.32 15.26 16.32 15.74 16.03 16.03C15.88 16.18 15.69 16.25 15.5 16.25C15.31 16.25 15.12 16.18 14.97 16.03L12 13.06L9.03 16.03C8.88 16.18 8.69 16.25 8.5 16.25C8.31 16.25 8.12 16.18 7.97 16.03C7.68 15.74 7.68 15.26 7.97 14.97L10.94 12L7.97 9.03C7.68 8.74 7.68 8.26 7.97 7.97C8.26 7.68 8.74 7.68 9.03 7.97L12 10.94L14.97 7.97C15.26 7.68 15.74 7.68 16.03 7.97C16.32 8.26 16.32 8.74 16.03 9.03L13.06 12L16.03 14.97Z"
+                fill="currentColor"
+              />
+            </svg>
+            <span>IP blocks</span>
           </Link>
-          <Link className="sidebar-item" to={dashboardLink('bss')}>
-            Analytics
+          <Link className="sidebar-item with-image" to={dashboardLink('bss')}>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M22 22H2C1.59 22 1.25 21.66 1.25 21.25C1.25 20.84 1.59 20.5 2 20.5H22C22.41 20.5 22.75 20.84 22.75 21.25C22.75 21.66 22.41 22 22 22Z"
+                fill="currentColor"
+              />
+              <path
+                d="M9.75 4V22H14.25V4C14.25 2.9 13.8 2 12.45 2H11.55C10.2 2 9.75 2.9 9.75 4Z"
+                fill="currentColor"
+              />
+              <path d="M3 10V22H7V10C7 8.9 6.6 8 5.4 8H4.6C3.4 8 3 8.9 3 10Z" fill="currentColor" />
+              <path
+                d="M17 15V22H21V15C21 13.9 20.6 13 19.4 13H18.6C17.4 13 17 13.9 17 15Z"
+                fill="currentColor"
+              />
+            </svg>
+            <span>Analytics</span>
           </Link>
-          <Link className="sidebar-item" to={dashboardLink('new-community-requests')}>
-            Community Requests
+          <Link className="sidebar-item with-image" to={dashboardLink('new-community-requests')}>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19.5 3.67C19.5 3.66 19.5 3.65 19.48 3.64C19.26 3.36 18.97 3.21 18.63 3.21C18.1 3.21 17.46 3.56 16.77 4.3C15.95 5.18 14.69 5.11 13.97 4.15L12.96 2.81C12.56 2.27 12.03 2 11.5 2C10.97 2 10.44 2.27 10.04 2.81L9.02 4.16C8.31 5.11 7.06 5.18 6.24 4.31L6.23 4.3C5.1 3.09 4.09 2.91 3.52 3.64C3.5 3.65 3.5 3.66 3.5 3.67C3.14 4.44 3 5.52 3 7.04V16.96C3 18.48 3.14 19.56 3.5 20.33C3.5 20.34 3.51 20.36 3.52 20.37C4.1 21.09 5.1 20.91 6.23 19.7L6.24 19.69C7.06 18.82 8.31 18.89 9.02 19.84L10.04 21.19C10.44 21.73 10.97 22 11.5 22C12.03 22 12.56 21.73 12.96 21.19L13.97 19.85C14.69 18.89 15.95 18.82 16.77 19.7C17.46 20.44 18.1 20.79 18.63 20.79C18.97 20.79 19.26 20.65 19.48 20.37C19.49 20.36 19.5 20.34 19.5 20.33C19.86 19.56 20 18.48 20 16.96V7.04C20 5.52 19.86 4.44 19.5 3.67ZM14 14.5H8C7.59 14.5 7.25 14.16 7.25 13.75C7.25 13.34 7.59 13 8 13H14C14.41 13 14.75 13.34 14.75 13.75C14.75 14.16 14.41 14.5 14 14.5ZM16 11H8C7.59 11 7.25 10.66 7.25 10.25C7.25 9.84 7.59 9.5 8 9.5H16C16.41 9.5 16.75 9.84 16.75 10.25C16.75 10.66 16.41 11 16 11Z"
+                fill="currentColor"
+              />
+            </svg>
+            <span>Community Requests</span>
           </Link>
         </div>
       </div>
