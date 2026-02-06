7635fdaf6f6cefb1123c07016d814965d7f36417
reallytiredofclowns <153442635+reallytiredofclowns@users.noreply.github.com>
2026-02-06 06:43:46 -0500

    Allow re-request of community if request not pending (#194)

--- core/community.go
+++ core/community.go
@@ -1306,6 +1306,24 @@ func DeleteUnusedCommunities(ctx context.Context, db *sql.DB, n uint, dryRun boo
 	return deleted, nil
 }
 
+func communityRequestExists(ctx context.Context, db *sql.DB, byUser, name string) (bool, error) {
+	var id int
+	if err := db.QueryRowContext(ctx, `
+			SELECT id
+			FROM community_requests
+			WHERE deleted_at IS NULL AND by_user = ? AND community_name_lc = ? AND denied_at IS NULL
+			LIMIT 1`,
+		byUser,
+		strings.ToLower(name),
+	).Scan(&id); err != nil {
+		if err != sql.ErrNoRows {
+			return false, err
+		}
+		return false, nil
+	}
+	return true, nil
+}
+
 type CommunityRequest struct {
 	ID              int             `json:"id"`
 	ByUser          string          `json:"byUser"`
@@ -1328,6 +1346,18 @@ func CreateCommunityRequest(ctx context.Context, db *sql.DB, byUser, name, note 
 			HTTPStatus: http.StatusConflict,
 			Code:       "community-already-exists",
 			Message:    "The community you're requesting to create already exists",
+		}
+	}
+
+	requested, err := communityRequestExists(ctx, db, byUser, name)
+	if err != nil {
+		return err
+	}
+	if requested {
+		return &httperr.Error{
+			HTTPStatus: http.StatusConflict,
+			Code:       "community-already-requested",
+			Message:    "You already have a pending request for that community",
 		}
 	}
 
