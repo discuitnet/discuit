c2c9f38f054b98c2cb8003b7c6626783b95e910e
reallytiredofclowns <153442635+reallytiredofclowns@users.noreply.github.com>
2026-04-21 10:23:49 -0400

    UI: add support for YouTube timestamps and playlists (#200)

--- ui/src/components/PostCard/embed.tsx
+++ ui/src/components/PostCard/embed.tsx
@@ -4,6 +4,28 @@ import { useInView } from 'react-intersection-observer';
 import { Post } from '../../serverTypes';
 
 const YoutubeEmbed = ({ url }: { url: string }) => {
+
+  // match the way YouTube parses timestamps:
+  // pure number: interpret as seconds
+  // xxxsyyym (either order, either can be missing): as xxx seconds + yyy minutes
+  // anything else: start at time 0
+  const timestampStringToNum = (s: string) => {
+    let n = Number(s);
+    if (Number.isInteger(n)) {
+      return n
+    }
+    let matchResult = s.match(/(?:(\d+)m)?(?:(\d+)s)?(?:(\d+)m)?/i)
+    if (matchResult === null || matchResult[0] != s) { // s is not xxxsyyym
+      return 0;
+    } else if (matchResult[1] && matchResult[3]) { // malformed; two minute values
+      return 0;
+    } else {
+      let seconds = parseInt(matchResult[2]) || 0;
+      let minutes = (parseInt(matchResult[1]) || 0) + (parseInt(matchResult[3]) || 0);
+      return minutes * 60 + seconds;
+    }
+  }
+
   // Render only if the div is in view.
   const [ref, inView] = useInView({
     rootMargin: '200px 0px',
@@ -37,19 +59,29 @@ const YoutubeEmbed = ({ url }: { url: string }) => {
     return null;
   }
 
-  let videoId = '';
+  let videoId = '', timestampString = '', playlist = '';
   const u = new URL(url);
+  const params = new URLSearchParams(u.search);
   if (['youtube.com', 'www.youtube.com', 'm.youtube.com'].includes(u.hostname)) {
     // fix embeds for shorts/live, which seem to only appear on youtube.com and not youtu.be
     const pathArray = u.pathname.split('/');
     if (pathArray.includes('shorts') || pathArray.includes('live')) {
       videoId = pathArray[2];
     } else {
-      const params = new URLSearchParams(u.search);
       videoId = params.get('v') || '';
     }
   } else if (u.hostname === 'youtu.be' || u.hostname === 'www.youtu.be') {
     videoId = u.pathname;
+  }
+  playlist = params.get('list') || '';
+  timestampString = params.get('t') || '';
+  let timestamp = timestampStringToNum(timestampString);
+  let additionalParams = '';
+  if (timestamp != 0) {
+    additionalParams = `?start=${timestamp}`
+  }
+  if (playlist != '') {
+    additionalParams += (additionalParams != '' ? '&' : '?') + `listType=playlist&list=${playlist}`
   }
 
   return (
@@ -63,7 +95,7 @@ const YoutubeEmbed = ({ url }: { url: string }) => {
             onLoad={() => setIframeLoaded(true)}
             width={size.width}
             height={size.height}
-            src={`https://www.youtube-nocookie.com/embed/${videoId}`}
+            src={`https://www.youtube-nocookie.com/embed/${videoId}${additionalParams}`}
             frameBorder="0"
             allow="accelerometer; autoplay;clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
